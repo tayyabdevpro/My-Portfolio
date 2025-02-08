d651b02b2709d314a94585e35d1d3a124fa8e6eb
tayyabaslam <tayyabaslam@gmail.com>
2025-02-08 13:46:15 +0500

    update 08/02/2025

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const burgerBtn = document.querySelector('.mobile-menu');
 const mobileNav = document.querySelector('.mobile-nav');
 const closeBtn = document.querySelector('.close-menu');
 const mobileLinks = document.querySelectorAll('.mobile-nav ul li a');
+const floatingButtonDiv = document.querySelector('.floating_button_div');
+const botScreen = document.querySelector('.bot_screen');
+const closeBot = document.querySelector('.close-bot');
 burgerBtn.addEventListener('click', () => {
     mobileNav.classList.remove('closed-menu');
     mobileNav.classList.add('open-menu');
@@ -16,3 +19,17 @@ mobileLinks.forEach(link => {
         mobileNav.classList.add('closed-menu');
     });
 });
+
+floatingButtonDiv.addEventListener('click', () => {
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        window.location.href = 'https://tayyabdevpro.github.io/chatbot-system/';
+    } else {
+        botScreen.style.display = 'block';
+        closeBot.addEventListener('click', (e) => {
+            if (e.target !== floatingButtonDiv && e.target !== botScreen) {
+                botScreen.style.display = 'none';
+            }
+        });
+    }
+})
+
